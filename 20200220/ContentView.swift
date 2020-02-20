f9f94d28c7d08c8b68559caec36a9c4230187645
Shelly <shelly980102@gmail.com>
2020-02-20 13:53:38 +0800

    Initial Commit

---
+++ 20200220/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20200220
+//
+//  Created by 蕭聿莘 on 2020/2/20.
+//  Copyright © 2020 Kazuku. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
