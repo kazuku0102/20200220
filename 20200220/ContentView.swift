c8f3abcb6bf439053fd4021cf0f78e94edd43bd3
Shelly <shelly980102@gmail.com>
2020-03-24 17:21:13 +0800

    practice

--- 20200220/ContentView.swift
+++ 20200220/ContentView.swift
@@ -10,12 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 0) {
+            HStack{
+            Image("Guardians02")
+                .resizable()
+                .frame(width: 200, height: 280, alignment: .bottom)
+            .opacity(0.45)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.blue,Color.red,Color.blue]), startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(30)
+            
+            Divider()
+            
+            Image("brunch2")
+            .resizable()
+                .frame(width: 200, height: 280, alignment: .bottom)
+            .cornerRadius(30)
+        }
+        
+        Divider()
+        
+        HStack{
+            Image("brunch2")
+            .resizable()
+                .frame(width: 200, height: 280, alignment:.bottom)
+            .cornerRadius(30)
+            
+            Divider()
+            
+            Image("Guardians02")
+            .resizable()
+                .frame(width: 200, height: 280, alignment: .bottom)
+            .cornerRadius(30)
+        }
+        
+        Divider()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
